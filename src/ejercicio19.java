e14d225f63819f063c0b03b3aa9698418323c25b
Rhabad <nicolaspizarro.450@gmail.com>
2023-10-09 16:27:28 -0300

    retorna milisegundos

---
+++ src/ejercicio19.java
@@ -0,0 +1,8 @@
+public class ejercicio19 {
+    public static void main (String[] args){
+        /*
+        * Enunciado: Crea una función que reciba días, horas, minutos y segundos (como enteros)
+        * y retorne su resultado en milisegundos.
+        */
+    }
+}
