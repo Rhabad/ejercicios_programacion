2880d4eb43e36024d97d869eb9b1bfeee63a63cf
Rhabad <nicolaspizarro.450@gmail.com>
2023-10-11 18:31:29 -0300

    convertir min, seg, dia a milisegundo

--- src/ejercicio19.java
+++ src/ejercicio19.java
@@ -4,5 +4,27 @@ public class ejercicio19 {
         * Enunciado: Crea una función que reciba días, horas, minutos y segundos (como enteros)
         * y retorne su resultado en milisegundos.
         */
+        int dias = 2;
+        int horas = 12;
+        int minutos = 30;
+        int segundos = 45;
+
+        long milisegundos = convertirTiempoAMilisegundos(dias, horas, minutos, segundos);
+
+        System.out.println("El tiempo en milisegundos es: " + milisegundos);
     }
+
+        public static long convertirTiempoAMilisegundos(int dias, int horas, int minutos, int segundos) {
+            long milisegundosPorDia = 24 * 60 * 60 * 1000; // Milisegundos en un día
+            long milisegundosPorHora = 60 * 60 * 1000; // Milisegundos en una hora
+            long milisegundosPorMinuto = 60 * 1000; // Milisegundos en un minuto
+            long milisegundosPorSegundo = 1000; // Milisegundos en un segundo
+
+            long totalMilisegundos = dias * milisegundosPorDia
+                    + horas * milisegundosPorHora
+                    + minutos * milisegundosPorMinuto
+                    + segundos * milisegundosPorSegundo;
+
+            return totalMilisegundos;
+        }
 }
