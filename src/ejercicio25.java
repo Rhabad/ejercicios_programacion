35016d5017084b3bfb583f0e3bd32c73ada57dc5
Rhabad <nicolaspizarro.450@gmail.com>
2023-12-19 00:05:52 -0300

    piedra papel tijera

--- src/ejercicio25.java
+++ src/ejercicio25.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ejercicio25 {
     public static void main(String[] args) {
         /*
@@ -10,10 +12,29 @@ public class ejercicio25 {
          * - Ejemplo. Entrada: [("R","S"), ("S","R"), ("P","S")]. Resultado: "Player 2".
          */
 
+        Scanner entrada = new Scanner(System.in);
+        //jugando
+        String jugador1;
+        String jugador2;
+
+        System.out.println("Juego del Piedra, Papel, Tijeras");
+        System.out.println("Jugador 1:");
+        jugador1 = entrada.next();
+        System.out.println("Jugador 1:");
+        jugador2 = entrada.next();
+
+        //quien gano?
+
+
+        entrada.close();
     }
 
-    private static void registrojugadas(){
-        System.out.println();
+    //ganador en la ronda
+    private static String ganador(String jug1, String jug2){
+        if (jug1.equals("piedra") && jug2.equals("tijeras")){
+            System.out.println("gano piedra");
+        }
+        return "";
     }
 
 }
